22087b6f5214804811ae9db62dda88eae4049271
Alexander Rubtsov <RubtsovAV@gmail.com>
2017-05-03 10:39:35 +0000

    Changed functional tests for testing without a test container

--- tests/run.sh
+++ tests/run.sh
@@ -21,22 +21,30 @@ if [[ $1 == "build" ]]; then
 fi
 
 
+echo "-----------"
+echo "Launching containers"
 docker_compose down
 
-echo
-echo "-----------"
-echo "Test ftp adapter with pure-ftpd"
+echo "Launching the vsftpd on port 221"
+docker_compose up -d vsftpd
+docker_compose run wait vsftpd:21 -t 30
+
+echo "Launching the pure-ftpd on port 222"
 docker_compose up -d pure-ftpd
 docker_compose run wait pure-ftpd:21 -t 30
-docker_compose run -e FTP_ADAPTER_HOST=pure-ftpd test
-docker_compose down
 
 echo
 echo "-----------"
 echo "Test ftp adapter with vsftpd"
-docker_compose up -d vsftpd
-docker_compose run wait vsftpd:21 -t 30
-docker_compose run -e FTP_ADAPTER_HOST=vsftpd test
+FTP_ADAPTER_PORT=221 vendor/bin/phpunit
+
+echo
+echo "-----------"
+echo "Test ftp adapter with pure-ftpd"
+FTP_ADAPTER_PORT=222 vendor/bin/phpunit
+
+echo "-----------"
+echo "Stop containers"
 docker_compose down
 
 echo
